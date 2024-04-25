50f967aa3d5d5290ee465f8df9220c19d55bd680
Mila Udot <liudmila.udot@chronosphere.io>
2024-04-24 17:04:38 -0700

    add readme

--- src/NumberOfConnectedComponents.java
+++ src/NumberOfConnectedComponents.java
@@ -37,6 +37,40 @@ public class NumberOfConnectedComponents {
         }
     }
 
+    public int numberOfConnectedComponentsDfs(Map<Integer, List<Integer>> adj) {
+        Set<Integer> visited = new HashSet<>();
+        int ret = 0;
+        for (Integer key : adj.keySet()) {
+            if (!visited.contains(key)) {
+                ret++;
+            }
+            dfs(adj, key, visited);
+        }
+        return ret;
+    }
+
+    public void dfs(Map<Integer, List<Integer>> adjList, int startVertex, Set<Integer> visited) {
+        Stack<Integer> stack = new Stack<>(); // Stack for DFS
+
+        stack.push(startVertex); // Push the start vertex onto the stack
+
+        while (!stack.isEmpty()) {
+            int currentVertex = stack.pop(); // Pop a vertex from the stack
+
+            // Visit the current vertex if it has not been visited before
+            if (!visited.contains(currentVertex)) {
+                visited.add(currentVertex);
+
+                // Push all adjacent vertices of the current vertex onto the stack
+                for (int neighbor : adjList.getOrDefault(currentVertex, Collections.emptyList())) {
+                    if (!visited.contains(neighbor)) {
+                        stack.push(neighbor);
+                    }
+                }
+            }
+        }
+    }
+
     // number of provinces test method
     // https://leetcode.com/problems/number-of-provinces
     public int findCircleNum(int[][] isConnected) {
@@ -52,6 +86,7 @@ public class NumberOfConnectedComponents {
                 }
             }
         }
-        return numberOfConnectedComponentsBfs(adj);
+        // return numberOfConnectedComponentsBfs(adj);
+        return numberOfConnectedComponentsDfs(adj);
     }
 }
