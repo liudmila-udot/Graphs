7b942f458ae9ddc5f42c705d446d300b33e4fe03
Mila Udot <liudmila.udot@chronosphere.io>
2024-11-26 16:37:25 -0800

    closest DashMart update

--- src/ClosestDashMart.java
+++ src/ClosestDashMart.java
@@ -4,6 +4,41 @@ import java.util.Queue;
 
 /**
  * https://leetcode.com/discuss/interview-question/1522955/Doordash-Onsite
+ * A DashMart is a warehouse run by DoorDash that houses items found in
+ * convenience stores, grocery stores, and restaurants. We have a city with open
+ * roads, blocked-off roads, and DashMarts.
+ * City planners want you to identify how far a location is from its closest
+ * DashMart.
+ * You can only travel over open roads (up, down, left, right).
+ * Locations are given in [row, col] format.
+ * Example:
+ * [
+ * # 0 1 2 3 4 5 6 7 8
+ * ['X', ' ', ' ', 'D', ' ', ' ', 'X', ' ', 'X'], # 0
+ * ['X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', 'X'], # 1
+ * [' ', ' ', ' ', 'D', 'X', 'X', ' ', 'X', ' '], # 2
+ * [' ', ' ', ' ', 'D', ' ', 'X', ' ', ' ', ' '], # 3
+ * [' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X'], # 4
+ * [' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X', 'X'] # 5
+ * ]
+ * ' ' represents an open road that you can travel over in any direction (up, down, left, or right).
+ * 'X' represents an blocked road that you cannot travel through.
+ * 'D' represents a DashMart.
+ * # list of pairs [row, col]
+ * locations = [
+ * [2, 2],
+ * [4, 0],
+ * [0, 4],
+ * [2, 6],
+ * ]
+ * answer = [1, 4, 1, 5]
+ * Implement Function:
+ * - get_closest_dashmart(city, locations)
+ * Provided:
+ * - city: List[str]
+ * - locations: List[List[int]]
+ * Return:
+ * - answer: List[int]
  */
 public class ClosestDashMart {
 
@@ -23,6 +58,9 @@ public class ClosestDashMart {
                 }
             }
         }
+
+        // for each next cell in BSF set distance as current + 1
+        // NOTE: we start with 'D' so we'll have to do the math to calculate actual distance as number
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             for (int[] offset : OFFSETS) {
@@ -51,20 +89,9 @@ public class ClosestDashMart {
     }
 
     public static void main(String[] args) {
-        char[][] grid = {
-                {'X', ' ', ' ', 'D', ' ', ' ', 'X', ' ', 'X'},
-                {'X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', 'X'},
-                {' ', ' ', ' ', 'D', 'X', 'X', ' ', 'X', ' '},
-                {' ', ' ', ' ', 'D', ' ', 'X', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X'},
-                {' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X', 'X'}};
+        char[][] grid = {{'X', ' ', ' ', 'D', ' ', ' ', 'X', ' ', 'X'}, {'X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', 'X'}, {' ', ' ', ' ', 'D', 'X', 'X', ' ', 'X', ' '}, {' ', ' ', ' ', 'D', ' ', 'X', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X'}, {' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X', 'X'}};
 
-        int[][] locations = {
-                {2, 2},
-                {4, 0},
-                {0, 4},
-                {2, 6},
-        };
+        int[][] locations = {{2, 2}, {4, 0}, {0, 4}, {2, 6},};
 
         System.out.println(Arrays.toString(distances(grid, locations)));
     }
